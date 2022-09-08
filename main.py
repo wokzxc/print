3cd31b7b3ba8825243273ae2c334cbbcb4c6b3b3
Krater <100773745+PaulsLD@users.noreply.github.com>
2022-09-08 06:29:09 +0000

    nothin

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print ("Fordeo")
+print ("RAIVIS NOMIRA")
+class Viesis:
+  def __init__(self,vards,parole):
+
+    self.vards = vards
+    self.parole = parole
+  
+  def drukaVardu(self):
+    print ("Viesis "+f"{self.vards}"+ " izveidots")
+  def drukaParoli(self):
+    print ("Lietotājs "+f"{self.parole}"+ " parole izveidota")
+
+vards="PASHKA"
+parole="BUBA228"
+viesis1 = Viesis(vards, parole)
+viesis1.drukaVardu()
+viesis1.drukaParoli()
+print ("JUMS IR 250 BEZRISKA FREESPIN but jus ir lohs un neka nebus")
+  
+  
